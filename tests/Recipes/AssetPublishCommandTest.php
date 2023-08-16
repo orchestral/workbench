d2fc5bda969ad5e83c9cc47de484807771bc7f88
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-16 15:33:55 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Recipes/AssetPublishCommandTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Orchestra\Workbench\Tests\Recipes;
+
+use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
+use Mockery as m;
+use Orchestra\Testbench\Concerns\WithWorkbench;
+use Orchestra\Testbench\TestCase;
+use Orchestra\Workbench\Recipes\AssetPublishCommand;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AssetPublishCommandTest extends TestCase
+{
+    use WithWorkbench;
+
+    /** @test */
+    public function it_can_resolve_correct_command()
+    {
+        $kernel = m::mock(ConsoleKernel::class);
+        $output = m::mock(OutputInterface::class);
+
+        $command = new AssetPublishCommand();
+
+        $kernel->shouldReceive('call')
+            ->once()
+            ->with(
+                'vendor:publish',
+                ['--tag' => ['workbench-assets', 'laravel-assets'], '--force' => true],
+                m::type(OutputInterface::class)
+            )->andReturnNull();
+
+        $command->handle($kernel, $output);
+    }
+}
