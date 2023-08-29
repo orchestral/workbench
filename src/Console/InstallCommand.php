44c6ab742b0c721abf1eeaa88c61ca2b67a42292
Mior Muhammad Zaki <crynobone@gmail.com>
2023-08-29 23:36:23 +0800

    Add new `workbench:devtool` command (#16)
    * Add new `workbench:devtool` command.
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    * wip
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>
    
    ---------
    
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -8,7 +8,6 @@ use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Orchestra\Testbench\Foundation\Console\Actions\EnsureDirectoryExists;
-use Orchestra\Testbench\Foundation\Console\Actions\GeneratesFile;
 use Orchestra\Workbench\Composer;
 use Orchestra\Workbench\Events\InstallEnded;
 use Orchestra\Workbench\Events\InstallStarted;
@@ -40,10 +39,7 @@ class InstallCommand extends Command
         $this->prepareWorkbenchDirectories($filesystem, $workingPath);
         $this->prepareWorkbenchNamespaces($filesystem, $workingPath);
 
-        $this->copyTestbenchConfigurationFile($filesystem, $workingPath);
-        $this->copyTestbenchDotEnvFile($filesystem, $workingPath);
-
-        $this->call('workbench:create-sqlite-db', ['--force' => true]);
+        $this->call('workbench:devtool', ['--force' => $this->option('force')]);
 
         return tap(Command::SUCCESS, function ($exitCode) use ($filesystem, $workingPath) {
             event(new InstallEnded($this->input, $this->output, $this->components, $exitCode));
@@ -197,88 +193,5 @@ class InstallCommand extends Command
         }
 
         return $content;
-    }
-
-    /**
-     * Copy the "testbench.yaml" file.
-     */
-    protected function copyTestbenchConfigurationFile(Filesystem $filesystem, string $workingPath): void
-    {
-        $from = (string) realpath(__DIR__.'/stubs/testbench.yaml');
-        $to = "{$workingPath}/testbench.yaml";
-
-        (new GeneratesFile(
-            filesystem: $filesystem,
-            components: $this->components,
-            force: (bool) $this->option('force'),
-        ))->handle($from, $to);
-    }
-
-    /**
-     * Copy the ".env" file.
-     */
-    protected function copyTestbenchDotEnvFile(Filesystem $filesystem, string $workingPath): void
-    {
-        $workbenchWorkingPath = "{$workingPath}/workbench";
-
-        $from = $this->laravel->basePath('.env.example');
-
-        if (! $filesystem->exists($from)) {
-            return;
-        }
-
-        $choices = Collection::make($this->environmentFiles())
-            ->filter(fn ($file) => ! $filesystem->exists("{$workbenchWorkingPath}/{$file}"))
-            ->values()
-            ->prepend('Skip exporting .env')
-            ->all();
-
-        if (! $this->option('force') && empty($choices)) {
-            $this->components->twoColumnDetail(
-                'File [.env] already exists', '<fg=yellow;options=bold>SKIPPED</>'
-            );
-
-            return;
-        }
-
-        $choice = $this->components->choice(
-            "Export '.env' file as?",
-            $choices,
-        );
-
-        if ($choice === 'Skip exporting .env') {
-            return;
-        }
-
-        $to = "{$workbenchWorkingPath}/{$choice}";
-
-        (new GeneratesFile(
-            filesystem: $filesystem,
-            components: $this->components,
-            force: (bool) $this->option('force'),
-        ))->handle($from, $to);
-
-        (new GeneratesFile(
-            filesystem: $filesystem,
-            force: (bool) $this->option('force'),
-        ))->handle((string) realpath(__DIR__.'/stubs/workbench.gitignore'), "{$workbenchWorkingPath}/.gitignore");
-    }
-
-    /**
-     * Get possible environment files.
-     *
-     * @return array<int, string>
-     */
-    protected function environmentFiles(): array
-    {
-        $environmentFile = \defined('TESTBENCH_DUSK') && TESTBENCH_DUSK === true
-            ? '.env.dusk'
-            : '.env';
-
-        return [
-            $environmentFile,
-            "{$environmentFile}.example",
-            "{$environmentFile}.dist",
-        ];
     }
 }
