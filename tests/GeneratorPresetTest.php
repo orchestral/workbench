fb272c5d7f09aa42b803244d7df7778b5e4bd1f7
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-18 20:39:34 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/GeneratorPresetTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Orchestra\Workbench\Tests;
+
+use BadMethodCallException;
+use Orchestra\Canvas\Core\PresetManager;
+use Orchestra\Canvas\Core\Presets\Preset;
+use Orchestra\Testbench\Concerns\WithWorkbench;
+use Orchestra\Testbench\TestCase;
+
+class GeneratorPresetTest extends TestCase
+{
+    use WithWorkbench;
+
+    /** @test */
+    public function it_can_be_resolved_and_has_correct_signature()
+    {
+        $workingPath = realpath(__DIR__.'/../workbench');
+
+        $preset = $this->app[PresetManager::class]->driver('workbench');
+
+        $this->assertInstanceOf(Preset::class, $preset);
+        $this->assertSame('workbench', $preset->name());
+
+        $this->assertSame($workingPath, $preset->basePath());
+        $this->assertSame($workingPath.DIRECTORY_SEPARATOR.'app', $preset->sourcePath());
+        $this->assertSame($workingPath.DIRECTORY_SEPARATOR.'resources', $preset->resourcePath());
+        $this->assertSame($workingPath.DIRECTORY_SEPARATOR.'resources/views', $preset->viewPath());
+        $this->assertSame($workingPath.DIRECTORY_SEPARATOR.'database/factories', $preset->factoryPath());
+        $this->assertSame($workingPath.DIRECTORY_SEPARATOR.'database/migrations', $preset->migrationPath());
+        $this->assertSame($workingPath.DIRECTORY_SEPARATOR.'database/seeders', $preset->seederPath());
+
+        $this->assertSame('Workbench\App\\', $preset->rootNamespace());
+        $this->assertSame('Workbench\App\Console\\', $preset->commandNamespace());
+        $this->assertSame('Workbench\App\Models\\', $preset->modelNamespace());
+        $this->assertSame('Workbench\App\Providers\\', $preset->providerNamespace());
+        $this->assertSame('Workbench\Database\Factories\\', $preset->factoryNamespace());
+        $this->assertSame('Workbench\Database\Seeders\\', $preset->seederNamespace());
+
+        $this->assertFalse($preset->hasCustomStubPath());
+        $this->assertSame('Illuminate\Foundation\Auth\User', $preset->userProviderModel());
+    }
+
+    /** @test */
+    public function it_cant_access_testing_path()
+    {
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage('Generating test is not supported for [workbench] preset');
+
+        $this->app[PresetManager::class]->driver('workbench')->testingPath();
+    }
+
+    /** @test */
+    public function it_cant_access_testing_namespace()
+    {
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage('Generating test is not supported for [workbench] preset');
+
+        $this->app[PresetManager::class]->driver('workbench')->testingNamespace();
+    }
+}
