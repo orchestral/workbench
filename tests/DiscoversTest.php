ea6e2b2f0665f090fa4f37967f9502d1889f7963
Mior Muhammad Zaki <crynobone@gmail.com>
2023-09-25 16:11:44 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/DiscoversTest.php
+++ tests/DiscoversTest.php
@@ -10,6 +10,13 @@ class DiscoversTest extends TestCase
     use WithWorkbench;
 
     /** @test */
+    public function it_can_resolve_web_routes_from_discovers()
+    {
+        $this->get('/hello')
+            ->assertOk();
+    }
+
+    /** @test */
     public function it_can_resolve_commands_from_discovers()
     {
         $this->artisan('workbench:inspire')->assertOk();
