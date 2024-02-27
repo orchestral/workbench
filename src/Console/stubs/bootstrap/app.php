223937fcd0d937539af3edb7ad91d1a5a9d2095c
Mior Muhammad Zaki <crynobone@gmail.com>
2024-02-27 19:46:09 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/stubs/bootstrap/app.php
+++ src/Console/stubs/bootstrap/app.php
@@ -4,7 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: $APP_BASE_PATH ?? Orchestra\Testbench\default_skeleton_path())
+use function Orchestra\Testbench\default_skeleton_path;
+
+return Application::configure(basePath: $APP_BASE_PATH ?? default_skeleton_path())
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
