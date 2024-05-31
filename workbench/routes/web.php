55360f9a548564363be24b53e4b028b00b34d08c
Mior Muhammad Zaki <crynobone@gmail.com>
2024-05-31 17:24:46 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('failed', fn () => throw new RuntimeException('Bad route!'));
+
+Route::get('/', fn () => view('welcome'))->name('welcome');
