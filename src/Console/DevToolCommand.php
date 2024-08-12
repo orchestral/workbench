6efeb1f057ac32b763b5675eb301c1379acd3d01
Mior Muhammad Zaki <crynobone@gmail.com>
2024-08-12 20:44:52 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/DevToolCommand.php
+++ src/Console/DevToolCommand.php
@@ -30,17 +30,17 @@ class DevToolCommand extends Command
     {
         $workingPath = package_path();
 
+        event(new InstallStarted($this->input, $this->output, $this->components));
+
+        $this->prepareWorkbenchDirectories($filesystem, $workingPath);
+        $this->prepareWorkbenchNamespaces($filesystem, $workingPath);
+
         if ($this->option('install') === true && $this->option('skip-install') === false) {
             $this->call('workbench:install', [
                 '--force' => $this->option('force'),
                 '--no-devtool' => true,
             ]);
         }
-
-        event(new InstallStarted($this->input, $this->output, $this->components));
-
-        $this->prepareWorkbenchDirectories($filesystem, $workingPath);
-        $this->prepareWorkbenchNamespaces($filesystem, $workingPath);
 
         return tap(Command::SUCCESS, function ($exitCode) use ($filesystem, $workingPath) {
             event(new InstallEnded($this->input, $this->output, $this->components, $exitCode));
