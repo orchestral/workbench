e9ac80c5d38286d94bffe840d896bc28aeff6bd9
Mior Muhammad Zaki <crynobone@gmail.com>
2024-10-23 15:51:24 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Events/InstallEndedTest.php
+++ tests/Events/InstallEndedTest.php
@@ -7,6 +7,8 @@ use Mockery as m;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase;
 use Orchestra\Workbench\Events\InstallEnded;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -14,11 +16,8 @@ class InstallEndedTest extends TestCase
 {
     use WithWorkbench;
 
-    /**
-     * @test
-     *
-     * @dataProvider basicInstallationDataProvider
-     */
+    #[Test]
+    #[DataProvider('basicInstallationDataProvider')]
     public function it_can_determine_using_basic_installation(bool $hasOption, ?bool $getOption, bool $expected)
     {
         $event = new InstallEnded(
