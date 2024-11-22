b2bbc600c891f2d4f7a355f28a0d8d49b393ddee
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-22 23:47:51 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -2,7 +2,7 @@
 
 namespace Orchestra\Workbench\Http\Controllers;
 
-abstract class Controller extends \Illuminate\Http\Controller
+abstract class Controller extends \Illuminate\Routing\Controller
 {
     //
 }
