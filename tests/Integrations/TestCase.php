110e42499b0d620f452ea9c0e78d8cf3974e5e8c
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-23 14:32:31 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integrations/TestCase.php
+++ tests/Integrations/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Orchestra\Workbench\Tests\Integrations;
 
-use Orchestra\Testbench\Attributes\WithConfig;
+use Illuminate\Contracts\Config\Repository;
 use Orchestra\Testbench\Attributes\WithMigration;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\Foundation\TestbenchServiceProvider;
@@ -10,13 +10,13 @@ use Orchestra\Testbench\Workbench\Workbench;
 use Orchestra\Workbench\WorkbenchServiceProvider;
 use Workbench\App\Models\User;
 
-#[WithConfig('auth.providers.users.model', User::class)]
 #[WithMigration]
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     use WithWorkbench;
 
     /** {@inheritDoc} */
+    #[\Override]
     protected function setUp(): void
     {
         parent::setUp();
@@ -29,6 +29,10 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function defineEnvironment($app)
     {
         Workbench::start($app, static::cachedConfigurationForWorkbench());
+
+        tap($app->make('config'), static function (Repository $config) {
+            $config->set('auth.providers.users.model', User::class);
+        });
     }
 
     /** {@inheritDoc} */
