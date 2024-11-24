4620564297312a885092aece3bd231783d67b6ea
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-25 07:06:27 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ import forms from '@tailwindcss/forms';
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
+        './vendor/orchestra/testbench-core/laravel/resources/views/**/*.blade.php',
         './resources/views/**/*.blade.php',
     ],
 
