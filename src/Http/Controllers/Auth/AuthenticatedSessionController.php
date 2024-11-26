f27c794dc9d6478e4b9b6b9c71cc34002bae7d0e
Mior Muhammad Zaki <crynobone@gmail.com>
2024-11-26 13:42:47 +0800

    [7.x] Allows to override the default redirect after logged-in
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ src/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        return redirect()->intended($this->redirectToAfterLoggedIn());
     }
 
     /**
