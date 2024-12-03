b51a7a17344d71515c39c9b55359a9b98bd83494
Mior Muhammad Zaki <crynobone@gmail.com>
2024-12-03 08:06:58 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Console/Concerns/InteractsWithFilesTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Orchestra\Workbench\Tests\Console\Concerns;
+
+use Illuminate\Filesystem\Filesystem;
+use Mockery as m;
+use Orchestra\Testbench\Concerns\InteractsWithMockery;
+use Orchestra\Workbench\Console\Concerns\InteractsWithFiles;
+use PHPUnit\Framework\TestCase;
+
+class InteractsWithFilesTest extends TestCase
+{
+    use InteractsWithMockery;
+
+    /** {@inheritDoc} */
+    #[\Override]
+    protected function tearDown(): void
+    {
+        $this->tearDownTheTestEnvironmentUsingMockery();
+
+        parent::tearDown();
+    }
+
+    /** @test */
+    public function it_can_replace_contents()
+    {
+        $filesystem = m::mock(Filesystem::class);
+
+        $fixture = new class($filesystem) {
+            use InteractsWithFiles;
+
+            public function __construct(public Filesystem $filesystem) {}
+            public function replace(array|string $search, array|string $replace, string $path) {
+                $this->replaceInFile($this->filesystem, $search, $replace, $path);
+            }
+        };
+
+        $filesystem->shouldReceive('get')->with('testbench.yaml')->once()->andReturn("laravel: '@testbench'")
+            ->shouldReceive('put')->with("laravel: '@testbench'", "laravel: '@testbench-dusk'", 'testbench.yaml');
+
+        $fixture->replace("laravel: '@testbench'", "laravel: '@testbench-dusk'", 'testbench.yaml');
+    }
+}
