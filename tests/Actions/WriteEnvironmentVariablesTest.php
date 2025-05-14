ab4e3e6e9b32198da05d6dbc8abc63ddd55c216c
Mior Muhammad Zaki <crynobone@gmail.com>
2025-05-14 23:29:48 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Actions/WriteEnvironmentVariablesTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Orchestra\Workbench\Tests\Actions;
+
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Filesystem\Filesystem;
+use Orchestra\Workbench\Actions\WriteEnvironmentVariables;
+
+class WriteEnvironmentVariablesTest extends TestCase
+{
+    /**
+     * @test
+     * @testWith [false]
+     *           [null]
+     */
+    public function it_throws_exception_when_env_file_is_not_available(mixed $filename)
+    {
+        $this->expectException(FileNotFoundException::class);
+
+        $filesystem = new Filesystem;
+
+        $action = new WriteEnvironmentVariables($filesystem, $filename);
+
+        $action->handle(['APP_NAME' => 'Laravel']);
+    }
+}
