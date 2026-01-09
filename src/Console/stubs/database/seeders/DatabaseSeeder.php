300d2600c8caaa1f1a52f846dff997c62621612c
Mior Muhammad Zaki <crynobone@gmail.com>
2026-01-09 12:24:06 +0800

    sync skeleton
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/stubs/database/seeders/DatabaseSeeder.php
+++ src/Console/stubs/database/seeders/DatabaseSeeder.php
@@ -2,12 +2,14 @@
 
 namespace Workbench\Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Orchestra\Testbench\Factories\UserFactory;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
