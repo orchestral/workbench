b4ec688da5f79252ab547544f16d6a2fe3b2ea30
Mior Muhammad Zaki <crynobone@gmail.com>
2026-08-26 11:28:55 +0800

    Update skeleton
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Console/stubs/bootstrap/app.php
+++ src/Console/stubs/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 use function Orchestra\Testbench\default_skeleton_path;
 
@@ -15,5 +16,7 @@ return Application::configure(basePath: $APP_BASE_PATH ?? default_skeleton_path(
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(
+            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
+        );
     })->create();
